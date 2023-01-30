962c923e5e4152b0f77cc117de48ec864bf67a49
+ <+>
2023-01-29 21:20:19 -0500

    commit tree 9557b38f181c6e79f8bd3f36920829e86f034e15

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,42 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemsUpdated }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearchInputText] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function onSearchChange(event) {
+    setSearchInputText(event.target.value)
+    console.log(event.target.value)
+  }
 
+  function onItemFormSubmit(newItem) {
+    onItemsUpdated(newItem)
+  }
+
+  const itemsToDisplay = items
+  .filter((item) => {
+    if (selectedCategory === "All") return true;
     return item.category === selectedCategory;
-  });
+  })
+  .filter((item) => {
+    if (search === "") {
+      return true
+    }
+
+    if (item.name.toLowerCase().includes(search.toLowerCase())) {
+      return true
+    }
+  })
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
